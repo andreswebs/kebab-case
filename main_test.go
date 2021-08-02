16ac606ff22fcab2b5935a9cdb477c34075f7a8e
Andre Silva <andreswebs@pm.me>
2021-08-01 22:54:26 -0300

    refactor

--- main_test.go
+++ main_test.go
@@ -1,36 +1,36 @@
 package main
 
-// import (
-// 	"testing"
-// )
+import (
+	"testing"
+)
 
-// func TestFormat(t *testing.T) {
-// 	cases := [][]string{
-// 		{"testCase", "test-case"},
-// 		{"TestCase", "test-case"},
-// 		{"Test Case", "test-case"},
-// 		{" Test Case", "test-case"},
-// 		{"Test Case ", "test-case"},
-// 		{" Test Case ", "test-case"},
-// 		{"test", "test"},
-// 		{"test-case", "test-case"},
-// 		{"Test", "test"},
-// 		{"", ""},
-// 		{"ManyManyWords", "many-many-words"},
-// 		{"manyManyWords", "many-many-words"},
-// 		{"AnyKind of-string", "any-kind-of-string"},
-// 		{"numbers2and55with000", "numbers2and55with000"},
-// 		{"JSONData", "jsondata"},
-// 		{"userID", "user-id"},
-// 		{"AAAbbb", "aaabbb"},
-// 		{"(test) case", "test-case"},
-// 	}
-// 	for _, i := range cases {
-// 		in := i[0]
-// 		out := i[1]
-// 		result := format(in)
-// 		if result != out {
-// 			t.Errorf("'%s' ('%s' != '%s')", in, result, out)
-// 		}
-// 	}
-// }
+func TestFormat(t *testing.T) {
+	cases := [][]string{
+		{"testCase", "test-case"},
+		{"TestCase", "test-case"},
+		{"Test Case", "test-case"},
+		{" Test Case", "test-case"},
+		{"Test Case ", "test-case"},
+		{" Test Case ", "test-case"},
+		{"test", "test"},
+		{"test-case", "test-case"},
+		{"Test", "test"},
+		{"", ""},
+		{"ManyManyWords", "many-many-words"},
+		{"manyManyWords", "many-many-words"},
+		{"AnyKind of-string", "any-kind-of-string"},
+		{"numbers2and55with000", "numbers2and55with000"},
+		{"JSONData", "jsondata"},
+		{"userID", "user-id"},
+		{"AAAbbb", "aaabbb"},
+		{"(test) case", "test-case"},
+	}
+	for _, i := range cases {
+		in := i[0]
+		out := i[1]
+		result := format(in)
+		if result != out {
+			t.Errorf("'%s' ('%s' != '%s')", in, result, out)
+		}
+	}
+}
